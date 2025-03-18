aa198ec0de57315c51b219426111a0708f396eca
Deniz Aktürk <deniz.akturk@hotmail.com>
2025-03-19 00:29:51 +0300

    New logger request and response functions

--- model.go
+++ model.go
@@ -1,6 +1,12 @@
 package logger
 
-import "time"
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+	"time"
+)
 
 type LogEntry struct {
 	Timestamp    time.Time      `json:"timestamp"`
@@ -19,4 +25,28 @@ type LoggedResponse struct {
 	StatusCode int                 `json:"status_code"`
 	Headers    map[string][]string `json:"headers"`
 	Body       interface{}         `json:"body"`
+}
+
+func NewLoggedRequest(r *http.Request) (LoggedRequest, error) {
+	bodyBytes, err := io.ReadAll(r.Body)
+	if err != nil {
+		return LoggedRequest{}, err
+	}
+
+	r.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+
+	return LoggedRequest{
+		Method:  r.Method,
+		URL:     r.URL.String(),
+		Headers: r.Header,
+		Body:    json.RawMessage(bodyBytes),
+	}, nil
+}
+
+func NewLoggedResponse(statusCode int, headers http.Header, body interface{}) LoggedResponse {
+	return LoggedResponse{
+		StatusCode: statusCode,
+		Headers:    headers,
+		Body:       body,
+	}
 }
